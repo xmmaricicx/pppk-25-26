9b952f8c90e8e2d3d5c8e6ede83ef086f0ea28ed
Marko <marko.maricic986@gmail.com>
2026-08-03 18:58:59 +0200

    feat: add seed data for lookup tables and doctors

--- backend/Pppk/Pppk.WebApi/HealthcareContext.cs
+++ backend/Pppk/Pppk.WebApi/HealthcareContext.cs
@@ -55,6 +55,14 @@ namespace Pppk.WebApi
 
             modelBuilder.Entity<Doctor>(entity =>
             {
+                entity.HasData(
+                     new Doctor { Id = 1, FirstName = "Ana", LastName = "Kovač", SpecialtyId = 1 },
+                     new Doctor { Id = 2, FirstName = "Marko", LastName = "Horvat", SpecialtyId = 2 },
+                     new Doctor { Id = 3, FirstName = "Ivana", LastName = "Novak", SpecialtyId = 3 },
+                     new Doctor { Id = 4, FirstName = "Petar", LastName = "Marić", SpecialtyId = 4 },
+                     new Doctor { Id = 5, FirstName = "Lucija", LastName = "Jurić", SpecialtyId = 5 },
+                     new Doctor { Id = 6, FirstName = "Tomislav", LastName = "Babić", SpecialtyId = 6 });
+
                 entity.HasOne(d => d.Specialty)
                 .WithMany(s => s.Doctors)
                 .HasForeignKey(d => d.SpecialtyId)
@@ -151,9 +159,81 @@ namespace Pppk.WebApi
                 .OnDelete(DeleteBehavior.Restrict);
             });
 
-            modelBuilder.Entity<Post>().HasIndex(p => p.PostalCode).IsUnique();
-            modelBuilder.Entity<ExaminationType>().HasIndex(e => e.Code).IsUnique();
-            modelBuilder.Entity<Specialty>().HasIndex(s => s.Name).IsUnique();
+            modelBuilder.Entity<Post>(entity =>
+            {
+                entity.HasData(
+                    new Post { Id = 1, PostalCode = "10000", City = "Zagreb" },
+                    new Post { Id = 2, PostalCode = "21000", City = "Split" },
+                    new Post { Id = 3, PostalCode = "51000", City = "Rijeka" },
+                    new Post { Id = 4, PostalCode = "31000", City = "Osijek" },
+                    new Post { Id = 5, PostalCode = "23000", City = "Zadar" },
+                    new Post { Id = 6, PostalCode = "10360", City = "Sesvete" });
+
+                entity.HasIndex(p => p.PostalCode).IsUnique();
+
+            });
+
+            modelBuilder.Entity<ExaminationType>(entity =>
+            {
+                entity.HasData(
+                    new ExaminationType { Id = 1, Code = "CT", Name = "Kompjuterizirana tomografija" },
+                    new ExaminationType { Id = 2, Code = "MR", Name = "Magnetska rezonanca" },
+                    new ExaminationType { Id = 3, Code = "ULTRA", Name = "Ultrazvuk" },
+                    new ExaminationType { Id = 4, Code = "EKG", Name = "Elektrokardiogram" },
+                    new ExaminationType { Id = 5, Code = "ECHO", Name = "Ehokardiogram" },
+                    new ExaminationType { Id = 6, Code = "OKO", Name = "Pregled oka" },
+                    new ExaminationType { Id = 7, Code = "DERM", Name = "Dermatološki pregled" },
+                    new ExaminationType { Id = 8, Code = "DENTA", Name = "Stomatološki pregled" },
+                    new ExaminationType { Id = 9, Code = "MAMMO", Name = "Mamografija" },
+                    new ExaminationType { Id = 10, Code = "EEG", Name = "Elektroencefalogram" });
+
+                entity.HasIndex(e => e.Code).IsUnique();
+            });
+
+            modelBuilder.Entity<Specialty>(entity =>
+            {
+                entity.HasData(
+                    new Specialty { Id = 1, Name = "Radiologija" },
+                    new Specialty { Id = 2, Name = "Kardiologija" },
+                    new Specialty { Id = 3, Name = "Oftalmologija" },
+                    new Specialty { Id = 4, Name = "Dermatologija" },
+                    new Specialty { Id = 5, Name = "Dentalna medicina" },
+                    new Specialty { Id = 6, Name = "Neurologija" });
+
+                entity.HasIndex(s => s.Name).IsUnique();
+            });
+
+            modelBuilder.Entity<Condition>().HasData(
+                new Condition { Id = 1, Name = "Hipertenzija" },
+                new Condition { Id = 2, Name = "Dijabetes tipa 2" },
+                new Condition { Id = 3, Name = "Astma" },
+                new Condition { Id = 4, Name = "Migrena" },
+                new Condition { Id = 5, Name = "Fibrilacija atrija" },
+                new Condition { Id = 6, Name = "Ekcem" },
+                new Condition { Id = 7, Name = "Gastritis" },
+                new Condition { Id = 8, Name = "Hipotireoza" });
+
+            modelBuilder.Entity<Medication>().HasData(
+                new Medication { Id = 1, Name = "Lisinopril" },
+                new Medication { Id = 2, Name = "Metformin" },
+                new Medication { Id = 3, Name = "Salbutamol" },
+                new Medication { Id = 4, Name = "Sumatriptan" },
+                new Medication { Id = 5, Name = "Bisoprolol" },
+                new Medication { Id = 6, Name = "Hidrokortizon" },
+                new Medication { Id = 7, Name = "Omeprazol" },
+                new Medication { Id = 8, Name = "Levotiroksin" });
+
+
+            modelBuilder.Entity<AddressType>(entity =>
+            {
+
+                entity.HasData(
+                    new AddressType { Id = 1, Name = "Boravište" },
+                    new AddressType { Id = 2, Name = "Prebivalište" });
+
+                entity.HasIndex(at => at.Name).IsUnique();
+            });
+
 
         }
 
